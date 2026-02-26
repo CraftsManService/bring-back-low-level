446cdfa75526b4393b0815df3a24147e4d864ef2
CraftsManService <153964515+CraftsManService@users.noreply.github.com>
2026-02-26 20:56:11 +0000

    f

--- src/opengl.cpp
+++ src/opengl.cpp
@@ -10,7 +10,36 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
-    GLFWwindow* window = glfwCreateWindow(480, 480, "MyWindow", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "MyWindow", nullptr, nullptr);
+    if(window == nullptr) {
+        std::cout << "Failed to create window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }   
+    glViewport(0, 0, 800, 600);
 
 
+    while (!glfwWindowShouldClose(window)) {
+        // Input
+        if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+
+        // Rendering commands
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Set background color
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
